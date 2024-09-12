ebef3ce66ef93f69f96df25108e5283c369be45c
Marcin Mikłas <m.miklas@wszib.edu.pl>
2024-09-12 22:10:18 +0200

    Initial commit

---
+++ src/main/java/pl/softem/springresthelloworld/controller/HelloWorldController.java
@@ -0,0 +1,36 @@
+package pl.softem.springresthelloworld.controller;
+
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HelloWorldController {
+
+    private List<String> elements = new ArrayList<>(Arrays.asList("Mleko", "Korba", "Pies"));
+
+    @GetMapping("/elements")
+    public String getElements() {
+        Gson gson = new Gson();
+        String jsonArray = gson.toJson(elements);
+        return jsonArray;
+    }
+
+    @PostMapping(value = "/elements")
+    public ResponseEntity<String> addElement(@RequestBody String element) {
+        elements.add(element);
+        return new ResponseEntity(element, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/elements/{index}")
+    public ResponseEntity<String> deleteElement(@PathVariable int index) {
+        elements.remove(index);
+        return new ResponseEntity("Element deleted", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/pl/softem/springresthelloworld/SpringRestHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.softem.springresthelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
